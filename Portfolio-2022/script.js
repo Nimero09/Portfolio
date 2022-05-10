4e20b8e95d52f620c19299a47a7163e9e991b769
Nimero09 <90343997+Nimero09@users.noreply.github.com>
2022-05-09 22:23:18 -0500

    Portfolio 2022

--- Portfolio-2022/script.js
+++ Portfolio-2022/script.js
@@ -52,6 +52,29 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
 	})
 })
 
+// Website Dark/Light theme
+
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+	document.body.classList.toggle("dark-theme");
+	themeBtn.classList.toggle("sun");
+
+	localStorage.setItem("saved-theme", getCurrentTheme());
+	localStorage.setItem("saved-icon", getCurrentIcon());
+})
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if(savedTheme) {
+	document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+	themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun")
+}
+
 // Scroll to top button
 
 const scrollTopBtn = document.querySelector(".scroll-btn");
@@ -64,25 +87,7 @@ scrollTopBtn.addEventListener("click", () => {
 	window.scroll(0, 0);
 })
 
-// Navigation menu items active on page scroll
 
-// window.addEventListener("scroll", () => {
-// 	const sections = document.querySelectorAll("section");
-// 	const scrollY = window.pageYOffSet;
-
-// 	sections.forEach(current => {
-// 		let sectionHeight = current.offsetHeight;
-// 		let sectionTop = current.offsetTop - 50;
-// 		let id = current.getAttribute("id");
-
-// 		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-// 			document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
-// 		}
-// 		else {
-// 			document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active")
-// 		}
-// 	})
-// })
 
 // Navigation menu items active on page scroll **** My method ****
 const sections = document.querySelectorAll("section");
@@ -102,7 +107,27 @@ window.addEventListener("scroll", () => {
 	navLi.forEach( li => {
 		li.classList.remove("active");
 		if(li.classList.contains(current)) {
-			li.classList.add("active")
+			li.classList.add("active");
 		}
 	})
 })
+
+// Alternate nav-menu highlight when scrolling code
+
+// window.addEventListener("scroll", () => {
+// 	const sections = document.querySelectorAll("section");
+// 	const scrollY = window.pageYOffSet;
+
+// 	sections.forEach(current => {
+// 		let sectionHeight = current.offsetHeight;
+// 		let sectionTop = current.offsetTop - 50;
+// 		let id = current.getAttribute("id");
+
+// 		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+// 			document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+// 		}
+// 		else {
+// 			document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active")
+// 		}
+// 	})
+// })
