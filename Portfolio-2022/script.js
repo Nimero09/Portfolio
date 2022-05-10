621c0cf1460f5b0c0e4f995aee1b68e689cf17b7
Nimero09 <90343997+Nimero09@users.noreply.github.com>
2022-05-09 21:43:19 -0500

    Portfolio 2022

--- Portfolio-2022/script.js
+++ Portfolio-2022/script.js
@@ -5,6 +5,7 @@ window.addEventListener("scroll", function(){
 })
 
 // Services section - modal
+
 const serviceModals = document.querySelectorAll(".service-modal");
 const learnMoreBtn = document.querySelectorAll(".learn-more-btn");
 const modalCloseBtn = document.querySelectorAll(".modal-close-btn");
@@ -28,6 +29,7 @@ modalCloseBtn.forEach((modalCloseBtn) => {
 })
 
 // Portfolio section modal 
+
 const portfolioModals = document.querySelectorAll(".portfolio-model");
 const imgCards = document.querySelectorAll(".img-card");
 const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
@@ -47,5 +49,60 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
 		portfolioModals.forEach((portfolioModalView) => {
 			portfolioModalView.classList.remove("active");
 		})
+	})
+})
+
+// Scroll to top button
+
+const scrollTopBtn = document.querySelector(".scroll-btn");
+
+window.addEventListener("scroll", function(){
+	scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+})
+
+scrollTopBtn.addEventListener("click", () => {
+	window.scroll(0, 0);
+})
+
+// Navigation menu items active on page scroll
+
+// window.addEventListener("scroll", () => {
+// 	const sections = document.querySelectorAll("section");
+// 	const scrollY = window.pageYOffSet;
+
+// 	sections.forEach(current => {
+// 		let sectionHeight = current.offsetHeight;
+// 		let sectionTop = current.offsetTop - 50;
+// 		let id = current.getAttribute("id");
+
+// 		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+// 			document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+// 		}
+// 		else {
+// 			document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active")
+// 		}
+// 	})
+// })
+
+// Navigation menu items active on page scroll **** My method ****
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".nav-items a")
+
+window.addEventListener("scroll", () => {
+	let current = ""
+	sections.forEach( section => {
+		const sectionTop = section.offsetTop - 50;
+		const sectionHeight = section.offsetHeight;
+
+		if(scrollY > sectionTop) {
+			current = section.getAttribute("id");
+		}
+	})
+
+	navLi.forEach( li => {
+		li.classList.remove("active");
+		if(li.classList.contains(current)) {
+			li.classList.add("active")
+		}
 	})
 })
