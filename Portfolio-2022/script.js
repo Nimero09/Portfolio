9cbbd74c6a12d751634c586b43c0c0ed94910e62
Nimero09 <90343997+Nimero09@users.noreply.github.com>
2022-05-12 23:08:56 -0500

    Portfolio 2022

--- Portfolio-2022/script.js
+++ Portfolio-2022/script.js
@@ -131,6 +131,28 @@ navItems.forEach((navItem) => {
 	navItem.addEventListener("click", close);
 })
 
+// Common reveal options to create reveal animations
+ScrollReveal({
+	reset: true,
+	distance: "60px",
+	duration: 2500,
+	delay: 100
+});
+
+// Target elements, and specify options to create reveal animations 
+
+ScrollReveal().reveal(".home .info h2, .section-title-01, .section-title-02", {delay: 500, origin: "left"});
+ScrollReveal().reveal(".home .info h3, .home .info p, .about-info .btn", { delay: 600, origin: "right" });
+ScrollReveal().reveal(".home .info .btn", { delay: 700, origin: "bottom" });
+ScrollReveal().reveal(".media-icons i, .contact-left li", { delay: 500, origin: "left", interval: 200 });
+ScrollReveal().reveal(".home-img, .about-img", { delay: 500, origin: "bottom" });
+ScrollReveal().reveal(".about .description, .contact-right", { delay: 600, origin: "right" });
+ScrollReveal().reveal(".about .professional-list li, .copy-right", { delay: 500, origin: "right", interval: 200 });
+ScrollReveal().reveal(".skills-description, .services-description, .contact-card, .contact-left h2", { delay: 700, origin: "left"});
+ScrollReveal().reveal(".experience-card, .service-card, .education, .portfolio .img-card", { delay: 800, origin: "bottom", interval: 200});
+ScrollReveal().reveal("footer .group", { delay: 500, origin: "top", interval: 200});
+
+
 // Alternate nav-menu highlight when scrolling code
 
 // window.addEventListener("scroll", () => {
