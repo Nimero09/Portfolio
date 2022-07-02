9307d47f99839fdcb1d978f78bdc2a1b8a0ca6fa
Nimero09 <90343997+Nimero09@users.noreply.github.com>
2022-07-01 20:45:25 -0500

    Model Agency Website

--- Model Agency Website/script.js
+++ Model Agency Website/script.js
@@ -6,3 +6,19 @@ window.addEventListener ("scroll", () => {
 	imgHero.classList.toggle("sticky", window.scrollY > 0);
 })
 
+
+// Menu btn toogle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const navigation = document.querySelector(".navigation")
+const menuClose = document.querySelector(".nav-close-btn");
+
+const openMenu = () => {
+	navigation.classList.toggle("active");
+}
+
+const closeMenu = () => {
+	navigation.classList.remove("active");
+}
+
+menuBtn.addEventListener("click", openMenu);
+menuClose.addEventListener("click", closeMenu);
